3649f7eb284183ba0300e84d5f5b92f91473b88f
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-06-07 15:20:35 -0300

    add echarts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import ReactEChartsCore from "echarts-for-react/lib/core";
 import './App.css';
+import echarts from './echarts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const option = {
+  xAxis: {
+    type: "category",
+  },
+  yAxis: {},
+  series: [
+    {
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+      type: "line"
+    }
+  ]
 }
 
+function App() {
+  return <ReactEChartsCore
+    echarts={echarts}
+    option={option}
+  />
+}
 export default App;
